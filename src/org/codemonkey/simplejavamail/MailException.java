39d8098489a75af7bd556cf2b03bdbd7becddc69
b.bottema@gmail.com <b.bottema@gmail.com@12a705a7-0299-ef8a-056f-5c7902ffcb57>
2011-03-21 13:19:35 +0000

    removes references to Vesijama

---
+++ src/org/codemonkey/simplejavamail/MailException.java
@@ -0,0 +1,29 @@
+package org.codemonkey.simplejavamail;
+
+import javax.mail.MessagingException;
+
+/**
+ * This exception is used to communicate errors during the sending of email.
+ * 
+ * @author Benny Bottema
+ */
+@SuppressWarnings("serial")
+public final class MailException extends RuntimeException {
+
+	protected static final String GENERIC_ERROR = "Generic error: %s";
+	protected static final String INVALID_ENCODING = "Encoding not accepted: %s";
+	protected static final String INVALID_RECIPIENT = "Invalid TO address: %s";
+	protected static final String INVALID_SENDER = "Invalid FROM address: %s";
+	protected static final String MISSING_SENDER = "Email is not valid: missing sender";
+	protected static final String MISSING_RECIPIENT = "Email is not valid: missing recipients";
+	protected static final String MISSING_SUBJECT = "Email is not valid: missing subject";
+	protected static final String MISSING_CONTENT = "Email is not valid: missing content body";
+
+	protected MailException(final String message) {
+		super(message);
+	}
+
+	protected MailException(final String message, final MessagingException cause) {
+		super(message, cause);
+	}
+}
