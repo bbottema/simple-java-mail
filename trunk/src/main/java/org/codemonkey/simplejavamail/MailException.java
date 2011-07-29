0a4533531e6f9af08d78a632143e27b29e51c081
b.bottema@gmail.com <b.bottema@gmail.com@12a705a7-0299-ef8a-056f-5c7902ffcb57>
2011-07-29 10:02:08 +0000

    added reply-to address

--- trunk/src/main/java/org/codemonkey/simplejavamail/MailException.java
+++ trunk/src/main/java/org/codemonkey/simplejavamail/MailException.java
@@ -15,6 +15,7 @@ public final class MailException extends RuntimeException {
 	protected static final String MISSING_USERNAME = "Can't have a password without username";
 	protected static final String INVALID_ENCODING = "Encoding not accepted: %s";
 	protected static final String INVALID_RECIPIENT = "Invalid TO address: %s";
+	protected static final String INVALID_REPLYTO = "Invalid REPLY TO address: %s";
 	protected static final String INVALID_SENDER = "Invalid FROM address: %s";
 	protected static final String MISSING_SENDER = "Email is not valid: missing sender";
 	protected static final String MISSING_RECIPIENT = "Email is not valid: missing recipients";
