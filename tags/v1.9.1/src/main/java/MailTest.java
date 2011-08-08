e1b02b7b3bc8845f81f515a435a6b221f01e8ed3
b.bottema@gmail.com <b.bottema@gmail.com@12a705a7-0299-ef8a-056f-5c7902ffcb57>
2011-08-08 12:18:05 +0000

    Updated Simple Java Mail v1.9 with Maven support (library hasn't changed), from trunk rev. 142

---
+++ tags/v1.9.1/src/main/java/MailTest.java
@@ -0,0 +1,41 @@
+import javax.mail.Message.RecipientType;
+
+import org.codemonkey.simplejavamail.Email;
+import org.codemonkey.simplejavamail.Mailer;
+import org.codemonkey.simplejavamail.TransportStrategy;
+
+/**
+ * Demonstration program for the Simple Java Mail framework.
+ * <p>
+ * <b>IMPORTANT</b>: <br />
+ * This testclass was designed to run from the commandline (or by Ant) and expects some system properties to be present. See
+ * <b>Readme.txt</b> for instructions. Alternatively, you can assign the host, username and password a hard value and ignore the system
+ * properties altogether.
+ * 
+ * @author Benny Bottema
+ */
+public class MailTest {
+
+	/**
+	 * Just run as Java application, but remember to set the JVM arguments first.
+	 * 
+	 * @param args should be empty, this demo uses JVM arguments only (-Dhost=value etc.).
+	 */
+	public static void main(final String[] args) {
+		final Email email = new Email();
+		email.setFromAddress("lollypop", "lol.pop@somemail.com");
+		email.addRecipient("C.Cane", "candycane@candyshop.org", RecipientType.TO);
+		email.setText("We should meet up!");
+		email.setTextHTML("<b>We should meet up!</b>");
+		email.setSubject("hey");
+		sendMail(email);
+	}
+
+	private static void sendMail(final Email email) {
+		final String host = System.getProperty("host") != null ? System.getProperty("host") : "";
+		final int port = System.getProperty("port") != null ? Integer.parseInt(System.getProperty("port")) : 25;
+		final String username = System.getProperty("username") != null ? System.getProperty("username") : "";
+		final String password = System.getProperty("password") != null ? System.getProperty("password") : "";
+		new Mailer(host, port, username, password, TransportStrategy.SMTP_SSL).sendMail(email);
+	}
+}
---
+++ tags/v1.9.1/src/main/java/org/codemonkey/simplejavamail/AttachmentResource.java
@@ -0,0 +1,50 @@
+package org.codemonkey.simplejavamail;
+
+import javax.activation.DataSource;
+
+/**
+ * A named immutable email attachment information object. The name can be a simple name, a filename or a named embedded
+ * image (eg. &lt;cid:footer&gt;). Contains a {@link DataSource} that is compatible with the javax.mail API.
+ * 
+ * @author Benny Bottema
+ * @see DataSource
+ */
+final class AttachmentResource {
+
+	/**
+	 * @see #AttachmentResource(String, DataSource)
+	 */
+	private final String name;
+
+	/**
+	 * @see #AttachmentResource(String, DataSource)
+	 */
+	private final DataSource dataSource;
+
+	/**
+	 * Constructor; initializes the attachment resource with a name and data.
+	 * 
+	 * @param name The name of the attachment which can be a simple name, a filename or a named embedded image (eg.
+	 *            &lt;cid:footer&gt;)
+	 * @param dataSource The attachment data.
+	 * @see DataSource
+	 */
+	public AttachmentResource(final String name, final DataSource dataSource) {
+		this.name = name;
+		this.dataSource = dataSource;
+	}
+
+	/**
+	 * Bean getter for {@link #dataSource}.
+	 */
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	/**
+	 * Bean getter for {@link #name}.
+	 */
+	public String getName() {
+		return name;
+	}
+}
