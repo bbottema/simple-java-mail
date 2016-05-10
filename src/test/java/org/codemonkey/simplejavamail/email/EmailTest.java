60c1d1d6f29b19b90d9fee32678e1efc4eb87d90
bbottema <b.bottema@projectnibble.org>
2016-05-10 19:57:56 +0200

    extracting dummy mail method for other future tests coming up

--- src/test/java/org/codemonkey/simplejavamail/email/EmailTest.java
+++ src/test/java/org/codemonkey/simplejavamail/email/EmailTest.java
@@ -23,7 +23,25 @@ public class EmailTest {
 	@Test
 	public void testParser()
 			throws IOException, MessagingException {
+		final Email emailNormal = createDummyEmail();
 
+		// let's try producing and then consuming a MimeMessage ->
+		final MimeMessage mimeMessage = Mailer.produceMimeMessage(emailNormal, Session.getDefaultInstance(new Properties()));
+		final Email emailFromMimeMessage = new Email(mimeMessage);
+
+		assertThat(emailFromMimeMessage, is(equalTo(emailNormal)));
+	}
+
+	@Test
+	public void testBeautifyCID() {
+		assertThat(Email.extractCID(null), is(nullValue()));
+		assertThat(Email.extractCID(""), is(""));
+		assertThat(Email.extractCID("haha"), is("haha"));
+		assertThat(Email.extractCID("<haha>"), is("haha"));
+	}
+
+	public static Email createDummyEmail()
+			throws IOException {
 		final Email emailNormal = new Email();
 		emailNormal.setFromAddress("lollypop", "lol.pop@somemail.com");
 		// don't forget to add your own address here ->
@@ -39,19 +57,6 @@ public class EmailTest {
 		emailNormal.addAttachment("location.txt", "On the moon!".getBytes(Charset.defaultCharset()), "text/plain");
 		String base64String = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABeElEQVRYw2NgoAAYGxu3GxkZ7TY1NZVloDcAWq4MxH+B+D8Qv3FwcOCgtwM6oJaDMTAUXOhmuYqKCjvQ0pdoDrCnmwNMTEwakC0H4u8GBgYC9Ap6DSD+iewAoIPm0ctyLqBlp9F8/x+YE4zpYT8T0LL16JYD8U26+B7oyz4sloPwenpYno3DchCeROsUbwa05A8eB3wB4kqgIxOAuArIng7EW4H4EhC/B+JXQLwDaI4ryZaDSjeg5mt4LCcFXyIn1fdSyXJQVt1OtMWGhoai0OD8T0W8GohZifE1PxD/o7LlsPLiFNAKRrwOABWptLAcqc6QGDAHQEOAYaAc8BNotsJAOgAUAosG1AFA/AtUoY3YEFhKMAvS2AE7iC1+WaG1H6gY3gzE36hUFJ8mqzbU1dUVBBqQBzTgIDQRkWo5qCZdpaenJ0Zx1aytrc0DDB0foIG1oAYKqC0IZK8D4n1AfA6IzwPxXpCFoGoZVEUDaRGGUTAKRgEeAAA2eGJC+ETCiAAAAABJRU5ErkJggg==";
 		emailNormal.addEmbeddedImage("thumbsup", parseBase64Binary(base64String), "image/png");
-
-		// let's try producing and then consuming a MimeMessage ->
-		final MimeMessage mimeMessage = Mailer.produceMimeMessage(emailNormal, Session.getDefaultInstance(new Properties()));
-		final Email emailFromMimeMessage = new Email(mimeMessage);
-
-		assertThat(emailFromMimeMessage, is(equalTo(emailNormal)));
-	}
-
-	@Test
-	public void testBeautifyCID() {
-		assertThat(Email.extractCID(null), is(nullValue()));
-		assertThat(Email.extractCID(""), is(""));
-		assertThat(Email.extractCID("haha"), is("haha"));
-		assertThat(Email.extractCID("<haha>"), is("haha"));
+		return emailNormal;
 	}
 }
