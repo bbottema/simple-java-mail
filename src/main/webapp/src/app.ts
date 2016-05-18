2e2e8289bb2c53789e7674ab8cb4669957861e02
bbottema <b.bottema@projectnibble.org>
2016-05-18 16:59:22 +0200

    no message

--- src/main/webapp/src/app.ts
+++ src/main/webapp/src/app.ts
@@ -1,7 +1,6 @@
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {provide, enableProdMode} from '@angular/core';
-import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 
 import {SimpleJavaMailApp} from './app/simple-java-mail-app';
@@ -9,7 +8,6 @@ import {SimpleJavaMailApp} from './app/simple-java-mail-app';
 // enableProdMode()
 
 bootstrap(SimpleJavaMailApp, [
-  HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 ])
