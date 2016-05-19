3c608f07335289af224c99270799ddde65209b34
bbottema <b.bottema@projectnibble.org>
2016-05-19 22:15:17 +0200

    Added Prism for syntax highlighting (but it isn't working yet)

--- src/main/webapp/src/app/simple-java-mail-app.ts
+++ src/main/webapp/src/app/simple-java-mail-app.ts
@@ -1,5 +1,7 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {Router, RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {Codeblock} from 'ng2-prism/codeblock';
+import {Java} from 'ng2-prism/languages';
 
 import {About} from './components/about/about';
 import {Features} from './components/features/features';
@@ -9,7 +11,7 @@ import {Contact} from './components/contact/contact';
 
 @Component({
   selector: 'simple-java-mail-app',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, Codeblock, Java],
   template: require('app/simple-java-mail-app.html'),
   styles: [require('app/simple-java-mail-app.less')],
   encapsulation: ViewEncapsulation.None
