4d1af396226a9d06e6c740eb5bea1120636716bb
bbottema <b.bottema@projectnibble.org>
2016-07-05 20:10:26 +0200

    Added a lot of documentation

--- src/main/webapp/src/app/components/features/features.ts
+++ src/main/webapp/src/app/components/features/features.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {SimplePageScroll, SimplePageScrollConfig} from 'ng2-simple-page-scroll';
+import {ROUTER_DIRECTIVES} from '@angular/router'; // needed for SimplePageScroll to nav to other views
 
 @Component({
   template: require('app/components/features/features.html'),
-  directives: [SimplePageScroll]
+  directives: [ROUTER_DIRECTIVES, SimplePageScroll]
 })
 
 export class Features {
