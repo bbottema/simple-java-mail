db258afb18fdf6e548033e1d5335ff0a4975cf87
bbottema <b.bottema@projectnibble.org>
2018-06-01 21:47:17 +0200

    Added helper method to perform precondition checks

--- src/main/java/org/simplejavamail/internal/util/Preconditions.java
+++ src/main/java/org/simplejavamail/internal/util/Preconditions.java
@@ -9,11 +9,18 @@ public final class Preconditions {
 	private Preconditions() {
 		// util / helper class
 	}
-
+	
 	public static <T> T checkNonEmptyArgument(@Nullable final T address, @Nonnull final String parameterName) {
 		if (MiscUtil.valueNullOrEmpty(address)) {
 			throw new IllegalArgumentException(format("%s is required", parameterName));
 		}
 		return address;
+	}
+	
+	public static boolean assumeTrue(boolean state, @Nonnull final String msg) {
+		if (!state) {
+			throw new IllegalArgumentException(msg);
+		}
+		return true;
 	}
 }
