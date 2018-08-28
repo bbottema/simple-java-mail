7ea77444d240a9e246cceb71c75a4bdfdde86ce8
bbottema <b.bottema@projectnibble.org>
2018-08-28 19:31:48 +0200

    minor improvements

--- src/main/java/org/simplejavamail/internal/clisupport/CliSupport.java
+++ src/main/java/org/simplejavamail/internal/clisupport/CliSupport.java
@@ -1,9 +1,7 @@
 package org.simplejavamail.internal.clisupport;
 
-import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.internal.clisupport.model.CliDeclaredOptionSpec;
 import org.simplejavamail.internal.clisupport.model.CliReceivedOptionData;
-import org.simplejavamail.mailer.MailerBuilder;
 import org.simplejavamail.mailer.MailerFromSessionBuilder;
 import picocli.CommandLine;
 import picocli.CommandLine.ParseResult;
@@ -11,20 +9,23 @@ import picocli.CommandLine.ParseResult;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.simplejavamail.email.EmailBuilder.*;
 import static org.simplejavamail.internal.clisupport.BuilderApiToPicocliCommandsMapper.generateOptionsFromBuilderApi;
+import static org.simplejavamail.internal.clisupport.CliCommandLineProducer.configurePicoCli;
+import static org.simplejavamail.mailer.MailerBuilder.*;
 
 public class CliSupport {
 	
-	private static final int TEXT_WIDTH = 150;
+	private static final int CONSOLE_TEXT_WIDTH = 150;
 	
-	private static final Class<?>[] RELEVANT_BUILDER_ROOT_API = {EmailBuilder.EmailBuilderInstance.class, MailerBuilder.MailerRegularBuilder.class, MailerFromSessionBuilder.class};
-	private static final List<CliDeclaredOptionSpec> COMMANDS_AND_OPTIONS = generateOptionsFromBuilderApi(RELEVANT_BUILDER_ROOT_API);
-	private static final CommandLine COMMAND_LINE = CliCommandLineProducer.configurePicoCli(COMMANDS_AND_OPTIONS, TEXT_WIDTH);
+	private static final Class<?>[] RELEVANT_BUILDER_ROOT_API 				= {EmailBuilderInstance.class, MailerRegularBuilder.class, MailerFromSessionBuilder.class};
+	private static final List<CliDeclaredOptionSpec> COMMANDS_AND_OPTIONS 	= generateOptionsFromBuilderApi(RELEVANT_BUILDER_ROOT_API);
+	private static final CommandLine COMMAND_LINE 							= configurePicoCli(COMMANDS_AND_OPTIONS, CONSOLE_TEXT_WIDTH);
 
 	public static void runCLI(String[] args) {
 		ParseResult pr = COMMAND_LINE.parseArgs(cutOffAtHelp(args));
 		
-		if (!CliCommandLineConsumerUsageHelper.processAndApplyHelp(pr, TEXT_WIDTH)) {
+		if (!CliCommandLineConsumerUsageHelper.processAndApplyHelp(pr, CONSOLE_TEXT_WIDTH)) {
 			List<CliReceivedOptionData> cliReceivedOptionData = CliCommandLineConsumer.consumeCommandLineInput(pr, COMMANDS_AND_OPTIONS);
 			CliCommandLineConsumerResultHandler.processCliResult(cliReceivedOptionData);
 		}
