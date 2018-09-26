3306ba90407cd36386ab0b93ae2059f7f30008a2
bbottema <b.bottema@projectnibble.org>
2018-09-26 21:45:08 +0200

    Added nullability annotations to CLI annotation

--- src/main/java/org/simplejavamail/internal/clisupport/model/CliBuilderApiType.java
+++ src/main/java/org/simplejavamail/internal/clisupport/model/CliBuilderApiType.java
@@ -1,14 +1,18 @@
 package org.simplejavamail.internal.clisupport.model;
 
+import javax.annotation.Nonnull;
+
 public enum CliBuilderApiType {
     EMAIL("email"), MAILER("mailer");
     
+    @Nonnull
     private final String paramPrefix;
     
-    CliBuilderApiType(String paramPrefix) {
+    CliBuilderApiType(@Nonnull String paramPrefix) {
         this.paramPrefix = paramPrefix;
     }
 	
+	@Nonnull
 	public String getParamPrefix() {
 		return paramPrefix;
 	}
