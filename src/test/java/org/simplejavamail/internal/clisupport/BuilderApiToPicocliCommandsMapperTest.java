aa7de86d7b28737a9333967f53586b69d97c2a29
bbottema <b.bottema@projectnibble.org>
2018-10-10 19:48:48 +0200

    Improved readability in case test fails

--- src/test/java/org/simplejavamail/internal/clisupport/BuilderApiToPicocliCommandsMapperTest.java
+++ src/test/java/org/simplejavamail/internal/clisupport/BuilderApiToPicocliCommandsMapperTest.java
@@ -6,29 +6,20 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.simplejavamail.internal.clisupport.BuilderApiToPicocliCommandsMapper.colorizeDescriptions;
 
 public class BuilderApiToPicocliCommandsMapperTest {
 	
 	@Test
-	public void colorizeDescriptions() {
-		List<String> strings = new ArrayList<>();
-		strings.add("nothing to colorize");
-		strings.add("one --item to colorize");
-		strings.add("item @|--already|@ colorized");
-		strings.add("@|item @|--already|@ c|@olorized");
-		strings.add("@|one @|--item|@ --to|@--colorize");
-		
-		List<String> colorizedStrings = BuilderApiToPicocliCommandsMapper.colorizeDescriptions(strings);
-		
-		assertThat(colorizedStrings).containsExactly(
-				"nothing to colorize",
-				"one @|cyan --item|@ to colorize",
-				"item @|--already|@ colorized",
-				"@|item @|--already|@ c|@olorized",
-				"@|one @|--item|@ --to|@@|cyan --colorize|@"
-		);
+	public void textColorizeDescriptions() {
+		assertThat(colorizeDescriptions(singletonList("nothing to colorize"))).containsExactly("nothing to colorize");
+		assertThat(colorizeDescriptions(singletonList("one --x:item to colorize"))).containsExactly("one @|cyan --x:item|@ to colorize");
+		assertThat(colorizeDescriptions(singletonList("item @|--x:already|@ colorized"))).containsExactly("item @|--x:already|@ colorized");
+		assertThat(colorizeDescriptions(singletonList("@|item @|--x:already|@ c|@olorized"))).containsExactly("@|item @|--x:already|@ c|@olorized");
+		assertThat(colorizeDescriptions(singletonList("@|one @|--x:item|@ --x:to|@--x:colorize"))).containsExactly("@|one @|--x:item|@ --x:to|@@|cyan --x:colorize|@");
 	}
 	
 	@Test
@@ -38,7 +29,7 @@ public class BuilderApiToPicocliCommandsMapperTest {
 		
 		assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
 			public void call() {
-				BuilderApiToPicocliCommandsMapper.colorizeDescriptions(strings);
+				colorizeDescriptions(strings);
 			}
 		})
 				.isInstanceOf(IllegalArgumentException.class)
@@ -52,7 +43,7 @@ public class BuilderApiToPicocliCommandsMapperTest {
 		
 		assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
 			public void call() {
-				BuilderApiToPicocliCommandsMapper.colorizeDescriptions(strings);
+				colorizeDescriptions(strings);
 			}
 		})
 				.isInstanceOf(IllegalArgumentException.class)
