22f327950a3b83aa3ff182217ebfee7ef8adbddf
bbottema <b.bottema@projectnibble.org>
2018-11-21 21:35:56 +0100

    removed unused args-checks

--- src/test/java/demo/CliListAllSupportedOptionsDemoApp.java
+++ src/test/java/demo/CliListAllSupportedOptionsDemoApp.java
@@ -6,11 +6,11 @@ import org.simplejavamail.internal.clisupport.model.CliDeclaredOptionSpec;
 public class CliListAllSupportedOptionsDemoApp {
 	public static void main(String[] args) {
 		long startMs = System.currentTimeMillis();
-		displayAllOptions(args);
+		displayAllOptions();
 		System.out.println(((System.currentTimeMillis() - startMs) / 1000d) + "ms");
 	}
 	
-	private static void displayAllOptions(String[] args) {
+	private static void displayAllOptions() {
 		for (CliDeclaredOptionSpec declaredOption : CliSupport.DECLARED_OPTIONS) {
 			CliSupport.runCLI(new String[]{ "send", declaredOption.getName() + "--help" });
 			System.out.print("\n\n\n\n");
