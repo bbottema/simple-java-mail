57ae0862de8b71f772891cadf5144335bc4891f6
bbottema <b.bottema@projectnibble.org>
2018-12-03 22:54:35 +0100

    expand cli demo's

--- src/test/java/demo/CliShowCommandHelpDemoApp.java
+++ src/test/java/demo/CliShowCommandHelpDemoApp.java
@@ -4,9 +4,11 @@ import org.simplejavamail.internal.modules.ModuleLoader;
 
 public class CliShowCommandHelpDemoApp {
 	public static void main(String[] args) {
-		ModuleLoader.loadCliModule().runCLI(new String[]{
-				"send",
-				"--help"
-		});
+		System.out.println("--------------- SEND -------------------");
+		ModuleLoader.loadCliModule().runCLI(new String[]{"send", "--help"});
+		System.out.println("\n\n\n\n\n\n-------------- CONNECT ------------------");
+		ModuleLoader.loadCliModule().runCLI(new String[]{"connect", "--help"});
+		System.out.println("\n\n\n\n\n\n------------- VALIDATE ------------------");
+		ModuleLoader.loadCliModule().runCLI(new String[]{"validate", "--help"});
 	}
 }
