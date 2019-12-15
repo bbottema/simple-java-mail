5adbb3c751d6f67f8520ef669685595cdf82d308
bbottema <b.bottema@projectnibble.org>
2019-12-15 11:02:33 +0100

    Improved CLI module javadoc

--- modules/cli-module/src/main/java/org/simplejavamail/cli/SimpleJavaMail.java
+++ modules/cli-module/src/main/java/org/simplejavamail/cli/SimpleJavaMail.java
@@ -5,8 +5,11 @@ import static org.simplejavamail.internal.clisupport.CliSupport.*;
 /**
  * Entry class when using the command line interface. Wires all the args into the CLI support.
  * <p>
- * For CLI usage help, simply add the {@code --help} flag or add it to any available option,
- * like so: {@code sjm send --mailer:async--help}.
+ * For CLI usage help, simply add the {@code --help} flag or add it to any available option, like so: <br>
+ * <strong>{@code sjm send --mailer:async--help}</strong>.
+ *
+ * @see <a href="http://www.simplejavamail.org/modules.html#cli-module">More about the CLI module</a>
+ * @see <a href="http://www.simplejavamail.org/cli.html#navigation">How to use simple Java Mail CLI</a>
  */
 public class SimpleJavaMail {
 	public static void main(String[] args) {
--- modules/cli-module/src/main/java/org/simplejavamail/cli/package-info.java
+++ modules/cli-module/src/main/java/org/simplejavamail/cli/package-info.java
@@ -1,7 +1,10 @@
 /**
- * Contains the only exposed API of this module which is the entry class to invoke from the command line.
+ * Contains the exposed API of this module which is the entry class to invoke from the command line.
  * <p>
  * For more information on this module, please refer to
  * <a href="http://www.simplejavamail.org/modules.html#cli-module">cli-module on simplejavamail.org</a>.
+ * <p>
+ * <strong>For more information on how to use the Simple Java Mail CLI, refer to:
+ * <a href="ttp://www.simplejavamail.org/cli.html#navigation">cli-module on simplejavamail.org</a>.</strong>
  */
 package org.simplejavamail.cli;
