ea65e068e6c0689a66d2882ee9538ec87310cd1e
bbottema <b.bottema@projectnibble.org>
2019-12-19 09:28:25 +0100

    Added junit test for Spring support

---
+++ modules/spring-module/src/test/java/org/simplejavamail/springsupport/SimpleJavaMailSpringSupportTest.java
@@ -0,0 +1,16 @@
+package org.simplejavamail.springsupport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { SimpleJavaMailSpringSupport.class })
+public class SimpleJavaMailSpringSupportTest {
+
+	@Test
+	public void testSpringPropertyPropagation() {
+
+	}
+}
