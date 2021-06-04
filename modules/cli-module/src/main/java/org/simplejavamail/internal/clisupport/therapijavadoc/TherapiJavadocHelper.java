ab7c01e00c0cc7891073d1d6d9b6adee749a7f0f
bbottema <b.bottema@projectnibble.org>
2021-06-04 20:47:17 +0200

    Manual quickfix on the Therapi code so that it won't break in Java8

--- modules/cli-module/src/main/java/org/simplejavamail/internal/clisupport/therapijavadoc/TherapiJavadocHelper.java
+++ modules/cli-module/src/main/java/org/simplejavamail/internal/clisupport/therapijavadoc/TherapiJavadocHelper.java
@@ -1,30 +1,36 @@
 package org.simplejavamail.internal.clisupport.therapijavadoc;
 
+import com.github.therapi.runtimejavadoc.ClassJavadoc;
 import com.github.therapi.runtimejavadoc.InlineLink;
 import com.github.therapi.runtimejavadoc.Link;
+import com.github.therapi.runtimejavadoc.MethodJavadoc;
 import com.github.therapi.runtimejavadoc.ParamJavadoc;
 import com.github.therapi.runtimejavadoc.RuntimeJavadoc;
 import com.github.therapi.runtimejavadoc.SeeAlsoJavadoc;
 import com.github.therapi.runtimejavadoc.Value;
 import org.bbottema.javareflection.ClassUtils;
 import org.bbottema.javareflection.MethodUtils;
-
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.regex.Pattern.compile;
+import static java.util.stream.Collectors.toList;
 import static org.simplejavamail.internal.clisupport.BuilderApiToPicocliCommandsMapper.colorizeDescriptions;
 import static org.simplejavamail.internal.util.ListUtil.getFirst;
 import static org.simplejavamail.internal.util.StringUtil.padRight;
 
 public final class TherapiJavadocHelper {
+
+	private static final Pattern LEGACY_PARAMETER_TYPE_PATTERN = compile("(?:.*:: )?([\\w.]+)\\)?");
 	
 	private TherapiJavadocHelper() {
 	}
@@ -59,12 +65,12 @@ public final class TherapiJavadocHelper {
 	@NotNull
 	static String getJavadocMainDescription(Method m, int nestingDepth) {
 		return new JavadocForCliFormatter(nestingDepth)
-				.format(RuntimeJavadoc.getJavadoc(m).getComment());
+				.format(getMethodJavadoc(m).getComment());
 	}
 	
 	@NotNull
 	public static List<DocumentedMethodParam> getParamDescriptions(Method m) {
-		List<ParamJavadoc> params = RuntimeJavadoc.getJavadoc(m).getParams();
+		List<ParamJavadoc> params = getMethodJavadoc(m).getParams();
 		if (m.getParameterTypes().length != params.size()) {
 			throw new AssertionError("Number of documented parameters doesn't match with Method's actual parameters: " + m);
 		}
@@ -74,7 +80,7 @@ public final class TherapiJavadocHelper {
 		}
 		return paramDescriptions;
 	}
-	
+
 	@NotNull
 	public static List<String> getJavadocSeeAlsoReferences(Method m, boolean onlyIncludeClicompatibleJavadocLinks, int maxTextWidth) {
 		List<String> seeAlsoReferences = new ArrayList<>();
@@ -84,7 +90,7 @@ public final class TherapiJavadocHelper {
 		int longestLink = 0;
 		boolean allDescriptionsOnNextLine = false;
 		
-		for (SeeAlsoJavadoc seeAlsoJavadoc : RuntimeJavadoc.getJavadoc(m).getSeeAlso()) {
+		for (SeeAlsoJavadoc seeAlsoJavadoc : getMethodJavadoc(m).getSeeAlso()) {
 			switch (seeAlsoJavadoc.getSeeAlsoType()) {
 				case STRING_LITERAL:
 					seeAlsoReferences.add(seeAlsoJavadoc.getStringLiteral());
@@ -126,7 +132,41 @@ public final class TherapiJavadocHelper {
 		
 		return seeAlsoReferences;
 	}
-	
+
+	/**
+	 * @deprecated this is a workaround for https://github.com/dnault/therapi-runtime-javadoc/issues/50
+	 */
+	@Deprecated
+	private static MethodJavadoc getMethodJavadoc(final Method m) {
+		ClassJavadoc javadoc = RuntimeJavadoc.getJavadoc(m.getDeclaringClass());
+		for (MethodJavadoc methodJavadoc : javadoc.getMethods()) {
+			if (matches(m, methodJavadoc)) {
+				return methodJavadoc;
+			}
+		}
+		return MethodJavadoc.createEmpty(m);
+	}
+
+	/**
+	 * @deprecated this is a workaround for https://github.com/dnault/therapi-runtime-javadoc/issues/50
+	 */
+	@Deprecated
+	private static boolean matches(final Method m, final MethodJavadoc methodJavadoc) {
+		if (!m.getName().equals(methodJavadoc.getName())) {
+			return false;
+		}
+		List<String> methodParamsTypes = new ArrayList<>();
+		for (Class<?> aClass : m.getParameterTypes()) {
+			methodParamsTypes.add(aClass.getCanonicalName());
+		}
+		// FIX (deprecated)
+		List<String> paramTypesStripped = methodJavadoc.getParamTypes().stream()
+				.map(s -> LEGACY_PARAMETER_TYPE_PATTERN.matcher(s).replaceFirst("$1"))
+				.collect(toList());
+		// /FIX
+		return methodParamsTypes.equals(paramTypesStripped);
+	}
+
 	@NotNull
 	public static List<String> determineCliOptionDescriptions(Method m) {
 		String javadoc = TherapiJavadocHelper.getJavadocMainDescription(m, 0);
