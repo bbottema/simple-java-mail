4822d7906730d54631c59ca2785f8e909e692291
bbottema <benny@bennybottema.com>
2023-12-15 15:20:15 +0100

    Added missing log statements to help determine which field was unequal betwen to Email instances

--- modules/core-module/src/main/java/org/simplejavamail/api/email/EqualsHelper.java
+++ modules/core-module/src/main/java/org/simplejavamail/api/email/EqualsHelper.java
@@ -30,7 +30,7 @@ public final class EqualsHelper {
         if (!fieldIsEqual(email1.getSentDate(), email2.getSentDate(), "sendDate")) {
             return false;
         }
-        if (!isEqualRecipientList(email1.getReplyToRecipients(), email2.getReplyToRecipients())) {
+        if (!isEqualRecipientList("ReplyTo", email1.getReplyToRecipients(), email2.getReplyToRecipients())) {
             return false;
         }
         if (!fieldIsEqual(email1.getBounceToRecipient(), email2.getBounceToRecipient(), "bounceToRecipient")) {
@@ -47,33 +47,40 @@ public final class EqualsHelper {
         }
         //noinspection SimplifiableConditionalExpression
         if (email1.getEmailToForward() != null ? email2.getEmailToForward() == null : email2.getEmailToForward() != null) {
+            log.debug("Email unqual for emailToForward: {} vs {}", email1.getEmailToForward(), email2.getEmailToForward());
             return false;
         }
         if (email1.getHTMLText() != null ? !normalizeNewlines(email1.getHTMLText()).equals(normalizeNewlines(email2.getHTMLText())) : email2.getHTMLText() != null) {
+            log.debug("Email unqual for HTML text: {} vs {}", email1.getHTMLText(), email2.getHTMLText());
             return false;
         }
         if (!fieldIsEqual(email1.getSubject(), email2.getSubject(), "subject")) {
             return false;
         }
-        if (!isEqualRecipientList(email1.getRecipients(), email2.getRecipients())) {
+        if (!isEqualRecipientList("TO/CC/BCC", email1.getRecipients(), email2.getRecipients())) {
             return false;
         }
-        if (!isEqualRecipientList(email1.getOverrideReceivers(), email2.getOverrideReceivers())) {
+        if (!isEqualRecipientList("Override receivers", email1.getOverrideReceivers(), email2.getOverrideReceivers())) {
             return false;
         }
         if (!email1.getEmbeddedImages().containsAll(email2.getEmbeddedImages()) || !email2.getEmbeddedImages().containsAll(email1.getEmbeddedImages())) {
+            log.debug("Email unqual for embedded images: {} vs {}", email1.getEmbeddedImages(), email2.getEmbeddedImages());
             return false;
         }
         if (!email1.getAttachments().containsAll(email2.getAttachments()) || !email2.getAttachments().containsAll(email1.getAttachments())) {
+            log.debug("Email unqual for attachments: {} vs {}", email1.getAttachments(), email2.getAttachments());
             return false;
         }
         if (!email1.getHeaders().equals(email2.getHeaders())) {
+            log.debug("Email unqual for headers: {} vs {}", email1.getHeaders(), email2.getHeaders());
             return false;
         }
         if (!Objects.equals(email1.getUseDispositionNotificationTo(), email2.getUseDispositionNotificationTo())) {
+            log.debug("Email unqual for useDispositionNotificationTo: {} vs {}", email1.getUseDispositionNotificationTo(), email2.getUseDispositionNotificationTo());
             return false;
         }
         if (!Objects.equals(email1.getUseReturnReceiptTo(), email2.getUseReturnReceiptTo())) {
+            log.debug("Email unqual for useReturnReceiptTo: {} vs {}", email1.getUseReturnReceiptTo(), email2.getUseReturnReceiptTo());
             return false;
         }
         if (!fieldIsEqual(email1.getDispositionNotificationTo(), email2.getDispositionNotificationTo(), "dispositionNotificationTo")) {
@@ -91,8 +98,9 @@ public final class EqualsHelper {
         return fieldIsEqual(email1.getReturnReceiptTo(), email2.getReturnReceiptTo(), "returnReceiptTo");
     }
 
-    private static boolean isEqualRecipientList(final List<Recipient> recipients, final List<Recipient> otherRecipients) {
+    private static boolean isEqualRecipientList(final String recipientType, final List<Recipient> recipients, final List<Recipient> otherRecipients) {
         if (recipients.size() != otherRecipients.size()) {
+            log.debug("Email unqual for [{}] recipients: {} vs {}", recipientType, recipients, otherRecipients);
             return false;
         }
         for (final Recipient otherRecipient : otherRecipients) {
