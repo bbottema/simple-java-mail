85c76979439bbd8084032ec162c25bc6b1ec76b9
bbottema <benny@bennybottema.com>
2026-08-07 23:33:11 +0200

    fix(mailer): reset connection pool max size

--- modules/simple-java-mail/src/test/java/org/simplejavamail/mailer/MailerBuilderTest.java
+++ modules/simple-java-mail/src/test/java/org/simplejavamail/mailer/MailerBuilderTest.java
@@ -1,7 +1,11 @@
 package org.simplejavamail.mailer;
 
 import org.junit.jupiter.api.Test;
+import org.simplejavamail.mailer.internal.MailerRegularBuilderImpl;
 import testutil.ConfigLoaderTestHelper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.simplejavamail.api.mailer.MailerGenericBuilder.DEFAULT_CONNECTIONPOOL_MAX_SIZE;
 
 public class MailerBuilderTest {
 	@Test
@@ -12,5 +16,17 @@ public class MailerBuilderTest {
 				.clearEmailValidator()
 				.buildMailer();
 		// good, no more errors due to #335
+	}
+
+	@Test
+	public void resetConnectionPoolMaxSizeRestoresOnlyMaxSize() {
+		ConfigLoaderTestHelper.clearConfigProperties();
+		final MailerRegularBuilderImpl builder = MailerBuilder.withSMTPServer("moo", 0)
+				.withConnectionPoolCoreSize(2)
+				.withConnectionPoolMaxSize(12)
+				.resetConnectionPoolMaxSize();
+
+		assertThat(builder.getConnectionPoolCoreSize()).isEqualTo(2);
+		assertThat(builder.getConnectionPoolMaxSize()).isEqualTo(DEFAULT_CONNECTIONPOOL_MAX_SIZE);
 	}
 }
